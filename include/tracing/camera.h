f6ce9944c9ff690c4074f2a7dcc97ac054403ac9
Andrew Huang <andyroiiid@live.com>
2021-07-18 22:29:06 +0800

    fix a defocus bug

--- include/tracing/camera.h
+++ include/tracing/camera.h
@@ -25,19 +25,19 @@ public:
         double viewportWidth = aspectRatio * viewportHeight;
 
         glm::dvec3 w = glm::normalize(lookFrom - lookAt);
-        glm::dvec3 u = glm::normalize(glm::cross(up, w));
-        glm::dvec3 v = glm::cross(w, u);
+        localXUnit = glm::normalize(glm::cross(up, w));
+        localYUnit = glm::cross(w, localXUnit);
 
         origin = lookFrom;
-        horizontal = focusDistance * viewportWidth * u;
-        vertical = focusDistance * viewportHeight * v;
+        horizontal = focusDistance * viewportWidth * localXUnit;
+        vertical = focusDistance * viewportHeight * localYUnit;
         lowerLeftCorner = origin - horizontal * 0.5 - vertical * 0.5 - focusDistance * w;
         lensRadius = aperture / 2.0;
     }
 
     [[nodiscard]] Ray getRay(double u, double v) const {
         glm::dvec2 random = lensRadius * randomInCircle();
-        glm::dvec3 newOrigin = origin + u * random.x + v * random.y;
+        glm::dvec3 newOrigin = origin + random.x * localXUnit + random.y * localYUnit;
         return {newOrigin, lowerLeftCorner + u * horizontal + v * vertical - newOrigin};
     }
 
@@ -46,6 +46,10 @@ private:
     glm::dvec3 horizontal{3.0, 0.0, 0.0};
     glm::dvec3 vertical{0.0, 3.0, 0.0};
     glm::dvec3 lowerLeftCorner{-1.5, -1.5, -1.0};
+
+    glm::dvec3 localXUnit{1.0, 0.0, 0.0};
+    glm::dvec3 localYUnit{0.0, 1.0, 0.0};
+
     double lensRadius = 0.0;
 };
 
